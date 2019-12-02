17371697dc99070dc586cf9caa3b32b5fe6ad55b
BellaBe <bella.belgarokova@hotmail.com>
2019-12-02 16:12:41 +0000

    remove files not in use, change README.md, commit project files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,114 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//jshint esversion: 6
+import React, { Component } from "react";
+import Chatkit from "@pusher/chatkit-client";
+import MessageList from "./components/MessageList";
+import SendMessageForm from "./components/SendMessageForm";
+import RoomList from "./components/RoomList";
+import NewRoomForm from "./components/NewRoomForm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import { tokenUrl, instanceLocator, userId } from "./config";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      roomId: null,
+      messages: [],
+      joinableRooms: [],
+      joinedRooms: []
+    };
+
+    this.sendMessage = this.sendMessage.bind(this);
+    this.subscribeToRoom = this.subscribeToRoom.bind(this);
+    this.getRooms = this.getRooms.bind(this);
+    this.createRoom = this.createRoom.bind(this);
+  }
+
+  componentDidMount() {
+    const tokenProvider = new Chatkit.TokenProvider({
+      url: tokenUrl
+    });
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator,
+      userId: userId,
+      tokenProvider: tokenProvider
+    });
+
+    chatManager
+      .connect()
+      .then(currentUser => {
+        this.currentUser = currentUser;
+        this.getRooms();
+      })
+      .catch(err => {
+        console.error("error on connecting: ", err);
+      });
+  }
+
+  getRooms() {
+    this.currentUser
+      .getJoinableRooms()
+      .then(joinableRooms => {
+        this.setState({
+          joinableRooms,
+          joinedRooms: this.currentUser.rooms
+        });
+      })
+      .catch(err => console.log("error on joinableRooms: ", err));
+  }
+
+  subscribeToRoom(roomId) {
+    this.setState({ messages: [] });
+    this.currentUser
+      .subscribeToRoomMultipart({
+        roomId: roomId,
+        hooks: {
+          onMessage: message => {
+            console.log("Received message:", message.parts[0].payload.content);
+            this.setState({
+              messages: [...this.state.messages, message]
+            });
+          }
+        }
+      })
+      .then(room => {
+        this.setState({
+          roomId: room.id
+        });
+        this.getRooms();
+      })
+      .catch(err => console.log("error on sbscribing to a room: ", err));
+  }
+  sendMessage(text) {
+    this.currentUser.sendSimpleMessage({
+      text: text,
+      roomId: this.state.roomId
+    });
+  }
+  createRoom(name){
+    console.log("Room name: ", name)
+    this.currentUser.createRoom({
+      name: name
+
+    })
+    .then(room =>this.subscribeToRoom(room.id))
+    .catch(err => console.log("Error on subscribing to a new room: ", err))
+  }
+  render() {
+    console.log("This.state.messages: ", this.state.messages);
+    return (
+      <div className="app">
+        <RoomList
+          roomId={this.state.roomId}
+          rooms={[...this.state.joinableRooms, ...this.state.joinedRooms]}
+          subscribeToRoom={this.subscribeToRoom}
+        />
+        <MessageList roomId ={this.state.roomId} messages={this.state.messages} />
+        <SendMessageForm disabled={!this.state.roomId} sendMessage={this.sendMessage} />
+        <NewRoomForm createRoom = {this.createRoom}/>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/components/Message.js
@@ -0,0 +1,13 @@
+//jshint esversion: 6
+import React from "react";
+
+const Message = props => {
+  const { username, text } = props;
+  return (
+    <div className="message">
+      <div className="message-username">{username}</div>
+      <div className="message-text">{text}</div>
+    </div>
+  );
+};
+export default Message;
